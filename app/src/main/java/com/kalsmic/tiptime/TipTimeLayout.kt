f44d4e3149eebf74ec2202dea636bd0495457c24
Arthur Kalule <kalulearthur@gmail.com>
2024-03-08 06:26:18 +0300

    add bill input

--- app/src/main/java/com/kalsmic/tiptime/TipTimeLayout.kt
+++ app/src/main/java/com/kalsmic/tiptime/TipTimeLayout.kt
@@ -3,21 +3,27 @@ package com.kalsmic.tiptime
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.kalsmic.tiptime.ui.theme.TipTimeTheme
 
 @Composable
 fun TipTimeLayout() {
@@ -37,6 +43,12 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
 
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+
+        )
         Text(
             text = stringResource(
                 id = R.string.tip_amount,
@@ -49,4 +61,19 @@ fun TipTimeLayout() {
 
     }
 
+}
+
+
+@Composable
+fun EditNumberField(modifier: Modifier){
+    var amountInput by remember { mutableStateOf("")}
+
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = { Text(text = stringResource(id = R.string.bill_amount)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
 }
